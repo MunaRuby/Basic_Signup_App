235b4457b3e0351d3cb55320adc792625f6256fb
Muna Ganobi <nwabundoonyekwelu@gmail.com>
2022-02-18 08:03:21 +0100

    edited signupdto

--- src/main/java/com/nwabundo/mkobo/controller/UserController.java
+++ src/main/java/com/nwabundo/mkobo/controller/UserController.java
@@ -5,26 +5,33 @@ import com.nwabundo.mkobo.dto.SignUpUserDto;
 import com.nwabundo.mkobo.payload.SignupResponseDTO;
 import com.nwabundo.mkobo.payload.UserResponseDto;
 import com.nwabundo.mkobo.service.UserService;
+import com.nwabundo.mkobo.serviceimpl.SignupErrorValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
 
     private final UserService userService;
+    private final SignupErrorValidationService validationService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, SignupErrorValidationService validationService) {
         this.userService = userService;
+        this.validationService = validationService;
     }
 
 
     @PostMapping("create")
-    public ResponseEntity<SignupResponseDTO> createuser(@RequestBody SignUpUserDto userDTO){
+    public ResponseEntity<?> createuser(@Valid @RequestBody SignUpUserDto userDTO, BindingResult result){
+        ResponseEntity<?> errorMap = validationService.validate(result);
+        if(errorMap != null) return errorMap;
         return ResponseEntity.ok().body(userService.createUser(userDTO));
 
     }
--- src/main/java/com/nwabundo/mkobo/dto/SignUpUserDto.java
+++ src/main/java/com/nwabundo/mkobo/dto/SignUpUserDto.java
@@ -3,16 +3,24 @@ package com.nwabundo.mkobo.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Email;
 
 
 @Data
 @AllArgsConstructor
 public class SignUpUserDto {
 
+    @NotBlank(message = "First name cannot be blank")
+    @Size(min = 2, message = "First name must have a minimum of 2 characters and above")
     private String firstName;
 
+    @NotBlank(message = "Last name cannot be blank")
+    @Size(min = 2, message = "Last name must have a minimum of 2 characters and above")
     private String lastName;
 
+    @Email(message = "Enter a valid Email")
     private String email;
 
     private String password;
